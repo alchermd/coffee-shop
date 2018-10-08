fa66f3d4577bccaba0afe972376ee94fb6de605f
John Alcher <johnalcherdoloiras@gmail.com>
2018-10-08 19:43:52 +0800

    Implement getters

--- src/coffeeshop/Product.java
+++ src/coffeeshop/Product.java
@@ -7,13 +7,25 @@ import javax.swing.JCheckBox;
  * Represents a product being sold.
  */
 class Product {
-    private String name;
-    private int price;
-    private JCheckBox checkBox;
+    private final String name;
+    private final int price;
+    private final JCheckBox checkBox;
     
     public Product(String name, int price, JCheckBox checkBox) {
         this.name = name;
         this.price = price;
         this.checkBox = checkBox;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public JCheckBox getCheckBox() {
+        return checkBox;
     }
 }
